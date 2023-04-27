2969ae2962688e786f0eae6bf600b0e24943952d
elmi.a <shahrokhelmi@gmail.com>
2023-04-27 16:14:29 +0330

    init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script>
+import MainNav from './components/MainNav.vue'
+export default {
+  name: 'App',
+  components: { MainNav }
+}
+</script>
+
+<template>
+  <MainNav />
+</template>
---
+++ src/components/MainNav.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="w-full text-sm bg-blue-50">
+    <div class="fixed top-0 left-0 h-16 bg-red-50 w-full">
+      <div class="h-full px-8 flex items-center text-xl border-solid border-brand-gray-1 border-b">
+        <a href="/">{{ company }}</a>
+      </div>
+    </div>
+  </header>
+  <a
+    href="
+  "
+  ></a>
+  <a href=""></a>
+</template>
+
+<script>
+export default {
+  name: 'MainNav',
+  data() {
+    return {
+      company: 'Boobo Careers',
+      author: 'Elmi'
+    }
+  }
+}
+</script>
