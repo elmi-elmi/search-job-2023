58c7ef0e7426c551f0d1249c02d0997fc4fce621
elmi.a <shahrokhelmi@gmail.com>
2023-05-12 15:06:00 +0330

    add healine test

--- src/components/TheHeadline.vue
+++ src/components/TheHeadline.vue
@@ -1,57 +1,63 @@
 <template>
-  <h1 class="mb-14 text-8xl font-bold tracking-tighter">
-    <span :class="actionClasses">{{ action }}</span>
-    <br />
-    for everyone
-  </h1>
-  <h2 class="text-3xl font-light">Find your next job at Bobo Corp.</h2>
+  <section>
+    <h1 class="mb-14 text-8xl font-bold tracking-tighter">
+      <span :class="actionClasses">{{ action }}</span>
+      <br />
+      for everyone
+    </h1>
+    <h2 class="text-3xl font-light">Find your next job at Bobo Corp.</h2>
+  </section>
 </template>
 
 <script>
-import nextElementInList from '@/utils/nextElementInList.js';
+import nextElementInList from "@/utils/nextElementInList";
+
 export default {
-  name: 'TheHealine',
+  name: "TheHeadline",
   data() {
     return {
-      action: 'Build',
-      actions: ['Build', 'Code', 'Create', 'Design'],
-      intervalId: null
-    }
+      action: "Build",
+      interval: null,
+    };
   },
   computed: {
     actionClasses() {
       return {
-        [this.action.toLowerCase()]: true
-      }
-    }
+        [this.action.toLowerCase()]: true,
+      };
+    },
   },
   created() {
-    this.changeHeadline()
+    this.changeTitle();
   },
   beforeUnmount() {
-    clearInterval(this.intervalId)
+    clearInterval(this.interval);
   },
   methods: {
-    changeHeadline() {
-      this.intervalId = setInterval(() => {
-        this.action = nextElementInList(this.actions, this.action)
-      }, 3000)
-    }
-  }
-}
+    changeTitle() {
+      this.interval = setInterval(() => {
+        const actions = ["Build", "Create", "Design", "Code"];
+        this.action = nextElementInList(actions, this.action);
+      }, 3000);
+    },
+  },
+};
 </script>
 
 <style scoped>
 .build {
   color: #1a73e8;
 }
-.code {
-  color: #d93025;
+
+.create {
+  color: #34a853;
 }
+
 .design {
   color: #f9ab00;
 }
-.create {
-  color: #34a853;
+
+.code {
+  color: #d93025;
 }
 </style>
--- tests/unit/utils/nextElementInList.test.js
+++ tests/unit/utils/nextElementInList.test.js
@@ -5,7 +5,7 @@ describe("nextElementInList",()=>{
     describe("When element is not at the end of the list",()=>{
         it("returns the next element of the list",()=>{
             const list = ['A', 'B', 'C', 'D', 'E'];
-            const current = 'C';
+            const current = 'C'; 
             const nextEl = nextElementInList(list, current)
             expect(nextEl).toBe('D')
         })
